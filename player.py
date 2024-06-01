8fab0022011996ec8fa181fe4b9674e69682e91c
Christian Thomas <christianthomas35@gmail.com>
2024-06-01 16:45:52 -0700

    starting to add play functionality
    broken, don't run yet

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import random
 
 class Player:
-    def __init__(self, name, position, skill_level, number=0):
+    def __init__(self, name, position, skill_level, number):
         self.name = name
         self.position = position
         self.skill_level = skill_level
@@ -9,22 +9,63 @@ class Player:
 
     # @staticmethod
     def generate_random_player(position):
+        o_nums = [100]
+        d_nums = [100]
+        number = 100
         # number generation
         match position:
-            case 'Quarterback': 
-                number = random.randrange(1, 19)
+            case 'Quarterback':
+                while number in o_nums:
+                    number = random.randrange(1, 19)
+                o_nums.append(number)
             case 'Running Back':
-                number = random.randrange(0,49)
+                while number in o_nums:
+                    number = random.randrange(0,49)
+                o_nums.append(number)
             case 'Wide Receiver':
-                number = random.choice([random.randint(0, 19), random.randint(80, 89)])
+                while number in o_nums:
+                    number = random.choice([random.randint(0, 19), random.randint(80, 89)])
+                o_nums.append(number)
+            case 'Tight End':
+                while number in o_nums:
+                    number = random.choice([random.randint(0, 19), random.randint(80, 89)])
+                o_nums.append(number)
+            case 'Offensive Tackle':
+                while number in o_nums:
+                    number = random.randrange(50, 79)
+                o_nums.append(number)
+            case 'Offensive Guard':
+                while number in o_nums:
+                    number = random.randrange(50, 79)
+                o_nums.append(number)
+            case 'Center':
+                while number in o_nums:
+                    number = random.randrange(50, 79)
+                o_nums.append(number)
+            case 'Edge':
+                while number in d_nums:
+                    number = random.randrange(0,99)
+                d_nums.append(number)
+            case 'Defensive Tackle':
+                while number in d_nums:
+                    number = random.randrange(0,99)
+                d_nums.append(number)
             case 'Linebacker':
-                number = random.choice([random.randrange(0,19), random.randint(30, 59)])
+                while number in d_nums:
+                    number = random.choice([random.randrange(0,19), random.randint(30, 59)])
+                d_nums.append(number)
             case 'Cornerback':
-                number = random.randrange(0,49)
-        
+                while number in d_nums:
+                    number = random.randrange(0,49)
+                d_nums.append(number)
+            case 'Safety':
+                while number in d_nums:
+                    number = random.randrange(0,49)
+                d_nums.append(number)
+
         return Player(
             name=f"{position} {number}",
             position=position,
-            skill_level=random.randint(50, 100),
+            skill_level=random.randint(50, 99),
             number={number}
         )
