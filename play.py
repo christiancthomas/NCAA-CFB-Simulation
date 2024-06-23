7d66f93c465a3110be74d7c9550e055028e1f8b0
Christian Thomas <christianthomas35@gmail.com>
2024-06-23 11:31:42 -0700

    def _engage()
    majorly simplified the logic for handling phase of play interactions

--- play.py
+++ play.py
@@ -9,7 +9,20 @@ class Play:
         self.yards_gained = None
         self.turnover = False
         self.type = type
-        self.players = None
+        self.players = {
+                    'ots': self.offense.get_players(position='Offensive Tackle'),
+                    'guards': self.offense.get_players(position='Offensive Guard'),
+                    'centers': self.offense.get_players(position='Center'),
+                    'qbs': self.offense.get_players(position='Quarterback'),
+                    'rbs': self.offense.get_players(position='Running Back'),
+                    'wrs': self.offense.get_players(position='Wide Receiver'),
+                    'tes': self.offense.get_players(position='Tight End'),
+                    'edges': self.defense.get_players(position='Edge'),
+                    'dts': self.defense.get_players(position='Defensive Tackle'),
+                    'lbs': self.defense.get_players(position='Linebacker'),
+                    'cbs': self.defense.get_players(position='Cornerback'),
+                    'safeties': self.defense.get_players(position='Safety'),
+                    }
         self.result = None
         self.phase = None
         # stats?
@@ -28,7 +41,7 @@ class Play:
 
     ### RUN GAME ###
     def _execute_run(self):
-        """executes a run play with three phases
+        """executes a run play with three phases. Uses _engage() to determine the winner of each engagement for each phase.
         1. backfield phase: QB hands off to RB.
             - There is a dice roll to determine who wins each blocking engagement.
             - If any of the defensive linemen wins, there is a dice roll to determine which winning lineman are in position to make a tackle (high chance).
@@ -56,13 +69,20 @@ class Play:
         self._backfield_phase()
         if self.yards_gained is not None:
             return self.yards_gained
-        self.phase = 'second level'
-        self._second_level_phase()
+        else:
+            self.phase = 'second level'
+            self._second_level_phase()
         if self.yards_gained is not None:
             return self.yards_gained
-        #tomporary -- delete 👇
         else:
-            return 0
+            self.yards_gained = 25
+            return self.yards_gained
+
+        # if self.yards_gained is not None:
+        #     return self.yards_gained
+        # #tomporary -- delete 👇
+        # else:
+        #     return 0
         # self.phase = 'open field'
         # self._open_field_phase()
         # if self.yards_gained is not None:
@@ -72,98 +92,12 @@ class Play:
     def _backfield_phase(self):
         """executes the backfield phase of a run play"""
         print('backfield phase')
-        winners = []
-        OTs = set(self.offense.get_players(position='Offensive Tackle'))
-        guards = set(self.offense.get_players(position='Offensive Guard'))
-        edge = set(self.offense.get_players(position='Edge'))
-        DTs = list(self.defense.get_players(position='Defensive Tackle'))
-        print(f"OTs: {[ ot.first_name+' '+ot.last_name for ot in OTs]},\n\
-                guards: {[g.first_name+' '+g.last_name for g in guards]},\n\
-                edge: {[e.first_name+' '+e.last_name for e in edge]},\n\
-                    DTs: {[dt.first_name+' '+dt.last_name for dt in DTs]}\n")
-        t_matchups = zip(OTs, edge)
-        i_matchups = zip(guards, DTs)
-
-        for ot, e in t_matchups:
-            print(f"Offensive tackle {ot.first_name} {ot.last_name}, {ot.rating} OVR vs. Edge: {e.first_name} {e.last_name}, {e.rating} OVR")
-            matchup = (ot.rating/100 * random.random() - e.rating/100 * random.random())
-            print(f"dice roll result: {matchup}")
-            if matchup > 0:
-                print('ot wins')
-                winner = ot
-            elif matchup < 0:
-                print('dt wins')
-                winner = e
-            else:
-                winner = [ot, e].sort(key=lambda x: x.rating, reverse=True)[0]
-            print('\n')
-            winners.append(winner)
-
-        for g, dt in i_matchups:
-            print(f"Offensive guard {g.first_name} {g.last_name}, {g.rating} OVR vs. Defensive Tackle: {dt.first_name} {dt.last_name}, {dt.rating} OVR")
-            matchup = (g.rating/100 * random.random() - dt.rating/100 * random.random())
-            print(f"dice roll result: {matchup}")
-            if matchup > 0:
-                print('guard wins')
-                winner = g
-            elif matchup < 0:
-                print('dt wins')
-                winner = dt
-            else:
-                winner = [g, dt].sort(key=lambda x: x.rating, reverse=True)[0]
-            print('\n')
-            winners.append(winner)
-        print(f"winners: {[winner.first_name+' '+winner.last_name for winner in winners]}")
-
-        for winner in winners:
-            # determine who gets a chance at making a tackle
-            tacklers = []
-            if winner in self.defense.get_players(position = ['Defensive Tackle', 'Edge']):
-                # 1. calculate if the winner is in right position to make a tackle
-                if random.random() < 0.8:
-                    tacklers.append(winner)
-        # 2. calculate if the tackler(s) makes the tackle
-        if len(tacklers) != 0:
-            for tackler in tacklers:
-                print(f"Defensive Lineman {tackler.first_name} {tackler.last_name}, {tackler.rating} OVR vs. Running Back: {self.offense.get_players(position='Running Back')[0].first_name} {self.offense.get_players(position='Running Back')[0].last_name}, {self.offense.get_players(position='Running Back')[0].rating} OVR")
-                matchup = (self.offense.get_players(position='Running Back')[0].rating/100 * random.random() - tackler.rating/100 * random.random())
-                if matchup < 0:
-                    print(f'{tackler.first_name} {tackler.last_name} tackles {self.offense.get_players(position='Running Back')[0].first_name} {self.offense.get_players(position='Running Back')[0].last_name}')
-                    print('tackle made, calculating yards gained...')
-                    self.yards_gained = self._yards_gained_helper(self.phase)
-                    break
-        else:
-            print('no tacklers in position')
-        # 3. calculate yards gained
-        return self.yards_gained
-
+        self._engage(self.offense.get_players(position='Running Back')[0], self.phase, **self.players)
 
     def _second_level_phase(self):
         """executes the second level phase of a run play"""
         print('second level phase')
-        winners = []
-        RB = self.offense.get_players(position='Running Back')[0]
-        LBs = self.defense.get_players(position='Linebacker').sort(key=lambda x: x.rating, reverse=True)
-        CBs = self.defense.get_players(position='Cornerback').sort(key=lambda x: x.rating, reverse=True)
-
-        for lb in LBs:
-            # 1. determine who gets a chance at making a tackle
-            tacklers = []
-            if random.random() < 0.5:
-                tacklers.append(lb)
-        # 2. calculate if the tackler(s) makes the tackle START HERE
-        if len(tacklers) != 0:
-            for lb in tacklers:
-                print(f"Linebacker {lb.first_name} {lb.last_name}, {lb.rating} OVR vs. Running Back: {RB.first_name} {RB.last_name}, {RB.rating} OVR")
-                matchup = (lb.rating/100 * random.random() - RB.rating/100 * random.random())
-                print(f"dice roll result: {matchup}")
-            if matchup > 0:
-                print('lb wins')
-                winner = lb
-            elif matchup < 0:
-                print('rb wins')
-                winner = RB
-
+        self._engage(self.offense.get_players(position='Running Back')[0], self.phase, **self.players)
 
     def _open_field_phase(self):
         ...
@@ -204,3 +138,106 @@ class Play:
             if min_val <= result <= max_val:
                 return round(result)
             # If result is outside the range, loop again
+
+
+
+    def _engage(self, ballcarrier, phase, **defenders):
+        """## Determines the winner of an engagement between the ballcarrier and one or more defenders.
+        ### Arguments
+        - ballcarrier: Player obj - the player with the ball.
+        - phase: str - the phase of the play.
+        - defenders: dict - a dictionary of defenders with keys for each position group.
+        ### Returns
+        - returns self.yards_gained if the ballcarrier is tackled.
+        - returns None if the ballcarrier is not tackled.
+        ### Usage details
+        - uses a weighted random number generator to determine the winner.
+        - weights are adjusted based on phase of play and player.
+        - uses _pick_from_bell_curve() to generate random number within desired range and weighting.
+
+        Phase	        Engagement      Weighting\n
+        Backfield	    -3 to 3         Bell Curve\n
+        Second Level	2 to 7          Bell Curve\n
+        Open Field	    8 to 99         Long Tail"""
+
+        winners = []
+        # backfield phase
+        if phase == 'backfield':
+            ots, edges = defenders['ots'], defenders['edges']
+            guards, dts = defenders['guards'], defenders['dts']
+            o_matchups = zip(ots, edges)
+            i_matchups = zip(guards, dts)
+
+            # engage guards and dts first
+            for g, dt in i_matchups:
+                matchup = (g.rating/100 * random.random() - dt.rating/100 * random.random())
+                if matchup > 0:
+                    winner = g
+                elif matchup < 0:
+                    winner = dt
+                else:
+                    winner = [g, dt].sort(key=lambda x: x.rating, reverse=True)[0]
+                winners.append(winner)
+
+            # engage tackles and edges
+            for ot, e in o_matchups:
+                matchup = (ot.rating/100 * random.random() - e.rating/100 * random.random())
+                if matchup > 0:
+                    winner = ot
+                elif matchup < 0:
+                    winner = e
+                else:
+                    winner = [ot, e].sort(key=lambda x: x.rating, reverse=True)[0]
+                winners.append(winner)
+
+            # determine who gets a chance at making a tackle
+            tacklers = []
+            for winner in winners:
+                if winner in self.defense.get_players(position = ['Defensive Tackle', 'Edge']):
+                    # 1. calculate if the winner is in right position to make a tackle
+                    if random.random() < 0.8:
+                        tacklers.append(winner)
+            if tacklers == []:
+                print('no defenders in position to make a tackle in backfield phase')
+            else:
+                print(f"{[t.first_name+" "+t.last_name for t in tacklers]} are in position to make a tackle.")
+
+            # 2. calculate if the tackler(s) makes the tackle
+            if len(tacklers) != 0:
+                for tackler in tacklers:
+                    matchup = (ballcarrier.rating/100 * random.random() - tackler.rating/100 * random.random())
+                    if matchup < 0:
+                        print(f"{tackler.first_name} {tackler.last_name} makes the tackle!")
+                        self.yards_gained = self._yards_gained_helper(phase)
+                        return self.yards_gained
+                    else:
+                        print('no tackle made in backfield phase')
+                        return None
+            else:
+                print('no defenders in position to make a tackle in backfield phase')
+
+        # second level phase
+        elif phase == 'second level':
+            lbs, cbs = defenders['lbs'], defenders['cbs']
+
+            # determine if defenders are in position to make a tackle
+            tacklers = []
+            for lb in lbs:
+                if random.random() < 0.5:
+                    tacklers.append(lb)
+            for cb in cbs:
+                if random.random() < 0.2:
+                    tacklers.append(cb)
+            print(f"{[t.first_name+" "+t.last_name for t in tacklers]} are in position to make a tackle.")
+            # determine if tackler(s) make the tackle
+            if len(tacklers) != 0:
+                for tackler in tacklers:
+                    matchup = (ballcarrier.rating/100 * random.random() - tackler.rating/100 * random.random())
+                    if matchup < 0:
+                        print(f"{tackler.first_name} {tackler.last_name} makes the tackle!")
+                        self.yards_gained = self._yards_gained_helper(phase)
+                        return self.yards_gained
+            else:
+                print('no tackle made in second level phase')
+                return None
+
